af93d5f70516492a9304db9d489257fa4d201753
AR384 <wjdqor0060@naver.com>
2025-02-05 13:03:08 +0900

    update

---
+++ JAVA_lec/lec1/src/My_Lab/_05_test_Method.java
@@ -0,0 +1,17 @@
+package My_Lab;
+
+public class _05_test_Method {
+    //prameter는 ,로 구분하여 여러개 받을 수 있음음
+    public static void testFunc(int num, int point) {
+        int pay = num * point;
+        System.out.println(pay);
+    }
+    public static String testFunc (int num) {
+        String nu3m = "s";
+        return nu3m;
+    }
+    public static void main(String[] args) {
+        
+    }
+
+}
